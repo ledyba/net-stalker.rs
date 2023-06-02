f75285f04c5e7b455d7b60e3773968d66c198b58
Kaede Fujisaki <kaede@hexe.net>
2023-06-02 18:36:28 +0900

    Graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod sites;
 
+use signal_hook::{consts::SIGINT, consts::SIGTERM, iterator::Signals};
 use axum::Extension;
 
 async fn root() -> &'static str {
@@ -8,6 +9,16 @@ async fn root() -> &'static str {
 
 fn main() -> anyhow::Result<()> {
   tracing_subscriber::fmt::init();
+  // Prepare signal handling.
+  let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+  let mut signals = Signals::new(&[SIGINT, SIGTERM])?;
+  std::thread::spawn(move || {
+    for _sig in signals.forever() {
+      tx.send(()).expect("Failed to send signal");
+      break;
+    }
+  });
+
   let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build()?;
   rt.block_on(async {
     use axum::{
@@ -19,8 +30,11 @@ fn main() -> anyhow::Result<()> {
       .route("/:name", get(sites::serve))
       .layer(Extension(sites::Service::new()));
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:3000".parse().expect("[BUG] Failed to parse addr"))
       .serve(app.into_make_service())
+      .with_graceful_shutdown(async {
+        rx.await.ok();
+      })
       .await?;
     Ok(())
   })

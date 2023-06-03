62651bade0438b9a04ad6dd356eb6f02c5ccac54
Kaede Fujisaki <kaede@hexe.net>
2023-06-04 08:35:44 +0900

    Fix compiling on Windows

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,21 @@ async fn root() -> &'static str {
 fn main() -> anyhow::Result<()> {
   tracing_subscriber::fmt::init();
 
+  #[cfg(not(windows))]
   // Prepare signal handling.
-  use signal_hook::iterator::Signals;
-  let (tx, rx) = tokio::sync::oneshot::channel::<()>();
-  let mut signals = Signals::new(signal_hook::consts::TERM_SIGNALS)?;
-  std::thread::spawn(move || {
-    for _sig in signals.forever() {
-      tx.send(()).expect("Failed to send signal");
-      break;
-    }
-  });
+  let rx = {
+    let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+
+    use signal_hook::iterator::Signals;
+    let mut signals = Signals::new(signal_hook::consts::TERM_SIGNALS)?;
+    std::thread::spawn(move || {
+      for _sig in signals.forever() {
+        tx.send(()).expect("Failed to send signal");
+        break;
+      }
+    });
+    rx
+  };
 
   let rt = tokio::runtime::Builder::new_multi_thread()
     .enable_all()
@@ -34,12 +39,14 @@ fn main() -> anyhow::Result<()> {
       .route("/:name", get(sites::serve))
       .layer(Extension(sites::Service::new()));
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().expect("[BUG] Failed to parse addr"))
-      .serve(app.into_make_service())
+    let server = axum::Server::bind(&"0.0.0.0:3000".parse().expect("[BUG] Failed to parse addr"))
+      .serve(app.into_make_service());
+    #[cfg(not(windows))]
+    let server = server
       .with_graceful_shutdown(async {
         rx.await.ok();
-      })
-      .await?;
+      });
+    server.await?;
     Ok(())
   })
 }

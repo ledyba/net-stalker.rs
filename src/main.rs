17d30fed53a685466872eeda473430ad93969d1f
Kaede Fujisaki <kaede@hexe.net>
2024-12-02 16:30:15 +0900

    Support windows termination

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,25 @@ fn main() -> anyhow::Result<()> {
     .finish()
     .init();
 
-  #[cfg(not(windows))]
-  // Prepare signal handling.
-  let rx = {
-    let (tx, rx) = tokio::sync::oneshot::channel::<()>();
-
-    use signal_hook::iterator::Signals;
-    let mut signals = Signals::new(signal_hook::consts::TERM_SIGNALS)?;
-    std::thread::spawn(move || {
-      for _sig in signals.forever() {
-        tx.send(()).expect("[BUG] Failed to send signal");
-        break;
-      }
-    });
-    rx
-  };
-
   let rt = tokio::runtime::Builder::new_multi_thread()
     .enable_all()
     .build()?;
+  let rt = std::sync::Arc::new(rt);
+
+  let shutdown_notify = {
+    use std::sync::Arc;;
+    let rt = Arc::clone(&rt);
+    let notify = Arc::new(tokio::sync::Notify::new());
+    let notify = Arc::clone(&notify);
+    let notifier = Arc::clone(&notify);
+    ctrlc::set_handler(move || {
+      let notify = Arc::clone(&notify);
+      rt.spawn(async move {
+        notify.notify_waiters();
+      });
+    })?;
+    notifier
+  };
 
   rt.block_on(async {
     use tracing::info;
@@ -49,18 +49,14 @@ fn main() -> anyhow::Result<()> {
       .route("/:name", get(sites::serve))
       .layer(Extension(sites::Service::new()));
 
-    let tcp_listener = tokio::net::TcpListener::bind(("0.0.0.0", 3000)).await.expect("[BUG] Failed to parse addr");
+    let tcp_listener = tokio::net::TcpListener::bind(("::", 3000)).await.expect("[BUG] Failed to parse addr");
     let socket_addr = tcp_listener.local_addr()?;
 
-    let server = axum::serve(tcp_listener, app);
-
-    #[cfg(not(windows))]
-    let server = {
-      let fut = async {
-        rx.await.expect("[BUG] Failed to recv signal.");
-      };
-      server.with_graceful_shutdown(fut)
-    };
+    let server =
+      axum::serve(tcp_listener, app)
+        .with_graceful_shutdown(async move {
+          shutdown_notify.notified().await;
+        });
 
     info!("Listening on http://localhost:{}/", socket_addr.port());
     server.await?;
